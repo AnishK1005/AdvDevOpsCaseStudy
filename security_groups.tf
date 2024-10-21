09a0ebe2c9d03d3a5f8ac05bfb8f26ae82fc2d13
AnishK1005 <2022.anish.kulkarni@ves.ac.in>
2024-10-21 19:22:42 +0530

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_security_group"
+  description = "Allow SSH and HTTP access for Jenkins"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sonarqube_sg" {
+  name        = "sonarqube_security_group"
+  description = "Allow SSH and HTTP access for SonarQube"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
